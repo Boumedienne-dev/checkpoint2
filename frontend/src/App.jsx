d183687ec10182feb411b82dc101c6dba4c370e6
Boumedienne-dev <bmessadia83@gmail.com>
2022-07-27 15:56:39 +0200

    fix error prettier v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "@components/NavBar";
 import Home from "@pages/Home";
 import CupcakeList from "@pages/CupcakeList";
+import CupcakeDtailspage from "@pages/CupcakeDetailspage";
 import Instructions from "@pages/Instructions";
 
 import "./App.css";
@@ -14,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/cupcakes" element={<CupcakeList />} />
           <Route path="/instructions" element={<Instructions />} />
+          <Route path="/list" element={<CupcakeList />} />
+          <Route path="/cupcakes/:id" element={<CupcakeDtailspage />} />
         </Routes>
       </main>
       <NavBar />
--- frontend/src/pages/CupcakeList.jsx
+++ frontend/src/pages/CupcakeList.jsx
@@ -1,9 +1,38 @@
-import Cupcake from "@components/Cupcake";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Cupcake from "../components/Cupcake";
+import axios from "axios";
 
 export default function CupcakeList() {
   // Step 1: get all cupcakes
 
+  const [cupcakes, setCupcakes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/cupcakes`)
+      .then((response) => response.data)
+      .then((data) => setCupcakes(data));
+  }, []);
+
   // Step 3: get all accessories
+
+  const [accessories, setAccessories] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/accessories`)
+      .then((response) => response.data)
+      .then((data) => setAccessories(data));
+  }, []);
+
+  const [accessoryFilter, setAccessoryFilter] = useState("");
+
+  const handleAccessoryFilter = (event) => {
+    event.preventDefault();
+
+    setAccessoryFilter(event.target.value);
+  };
 
   return (
     <>
@@ -11,17 +40,36 @@ export default function CupcakeList() {
       <form className="center">
         <label htmlFor="cupcake-select">
           Filter by{" "}
-          <select id="cupcake-select">
+          <select id="cupcake-select" onChange={handleAccessoryFilter}>
             <option value="">---</option>
             {/* Step 4: add an option for each accessory */}
+
+            {accessories &&
+              accessories.map((accessory) => (
+                <option key={accessory.id} value={accessory.id}>
+                  {accessory.name}
+                </option>
+              ))}
           </select>
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
-        <li className="cupcake-item">
-          <Cupcake />
-        </li>
+
+        {cupcakes &&
+          cupcakes
+            .filter(
+              (cupcake) =>
+                !accessoryFilter || cupcake.accessory_id === accessoryFilter
+            )
+            .map((cupcake) => (
+              <Link to={`/cupcakes/${cupcake.id}`}>
+                <li className="cupcake-item">
+                  <Cupcake key={cupcake.id} cupcake={cupcake} />
+                </li>
+              </Link>
+            ))}
+
         {/* end of block */}
       </ul>
     </>
